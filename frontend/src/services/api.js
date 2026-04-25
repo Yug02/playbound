a442bea0b252bba20a0a01dfe8ef18cdf61d529e
Yug <yuglakhera04@gmail.com>
2026-04-25 23:25:35 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-    baseURL: 'http://localhost:5000/api' 
+    baseURL: 'https://playbound-backend.onrender.com' 
 });
 
 API.interceptors.request.use((config) => {
